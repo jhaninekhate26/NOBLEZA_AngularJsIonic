33ec76ce48efe9e31ebf06db2725a9979f0787f9
Janine Kate Nobleza <Janine Kate Nobleza>
2017-02-10 00:31:39 +0800

    Using Angular Controllers

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -7,18 +7,19 @@ angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
+
+.controller('ListController', ['$scope', '$http', function($scope, $http) {
+    $http.get('js/data.json').success(function(data) {
+      $scope.artists = data;  
+    });
+}]);
